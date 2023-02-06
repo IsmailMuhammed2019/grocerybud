f25c199d875848278e398bb0b9c163a6583d5af6
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-02-06 14:21:56 +0100

    map through list array and create button for delete and edit in list.js

--- src/List.js
+++ src/List.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
+const List = ({items}) => {
   return (
     <div className="grocery-list">
-      <h3>List Component</h3>
+      {items.map((item) => {
+        const {id, title} = item
+        return (
+          <article key={id} className='grocery-item'>
+            <p className='title'>{title}</p>
+            <div className='btn-container'>
+              <button className='edit-btn'>
+                <FaEdit />
+              </button>
+              <button className='delete-btn'>
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        )
+      })}
     </div>
   )
 }
