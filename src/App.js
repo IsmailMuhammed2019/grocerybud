14a669a8681b2e8543ba3585110f009b6f4316fd
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-02-06 15:15:01 +0100

    pass removeAlert as a prop with value of showAlert function

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
    return (
      <section className='section-center'>
        <form onSubmit={handleSubmit} className='grocery-form'>
-         {alert.show && <Alert {...alert}/>}
+         {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
          <h3>Grocery List</h3>
          <div className='form-control'>
            <input
