5af7e9eb1f1504c5ea62c88b712b32018065ac6e
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-02-06 11:39:27 +0100

    setup grocery container for list

--- src/List.js
+++ src/List.js
@@ -2,24 +2,7 @@ import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
 const List = ({items}) => {
   return (
-    <div className="grocery-list">
-      {items.map((item) => {
-        const {id, title} = item
-        return (
-          <article key={id} className='grocery-item'>
-            <p className='title'>{title}</p>
-            <div className='btn-container'>
-              <button type='button' className='edit-btn'>
-                <FaEdit />
-              </button>
-              <button type='button' className='delete-btn'>
-                <FaTrash />
-              </button>
-            </div>
-          </article>
-        )
-      })}
-    </div>
+    <h3>List Goes here</h3>
   )
 }
 
