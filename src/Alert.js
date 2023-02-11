9f7218dfe95a9ec3e8a018014622585b37003bfa
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-02-11 20:25:44 +0100

    set remove alert function

--- src/Alert.js
+++ src/Alert.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 
-const Alert = ({type, msg}) => {
+const Alert = ({type, msg, removeAlert}) => {
+
+  
   return (
     <p className={`alert alert-${type}`}>{msg}</p>
   )
