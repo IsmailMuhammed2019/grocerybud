867e33721d326d5e3123940e5ac48096ea8ecc62
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-02-11 17:50:14 +0100

    new state of app

--- src/Alert.js
+++ src/Alert.js
@@ -1,14 +1,8 @@
 import React, { useEffect } from 'react'
 
 const Alert = ({msg, type, removeAlert, list }) => {
-  useEffect(() => {
-    const timeOut = setTimeout(() => {
-      removeAlert()
-    },3000)
-    return (() => clearTimeout(timeOut))
-  },[list])
-  return ( 
-    <p className={`alert alert-${type}`}>{msg}</p>
+  return (
+    <p>alert page</p>
   )
 }
 
--- src/List.js
+++ src/List.js
@@ -1,25 +1,9 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({items, removeItem, editItem}) => {
+const List = () => {
+  
   return (
-    <div className="grocery-list">
-      {items.map((item) => {
-        const {id, title} = item
-        return (
-          <article key={id} className='grocery-item'>
-            <p className='title'>{title}</p>
-            <div className='btn-container'>
-              <button className='edit-btn' type='button' onClick={() => editItem(id)}>
-                <FaEdit />
-              </button>
-              <button className='delete-btn' type='button' onClick={() => removeItem(id)} >
-                <FaTrash />
-              </button>
-            </div>
-          </article>
-        )
-      })}
-    </div>
+    <p>list page</p>
   )
 }
 
