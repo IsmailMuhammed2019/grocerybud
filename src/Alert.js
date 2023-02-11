b8b7ca60e676724621996ddb0490c177013d0ac1
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-02-11 20:26:54 +0100

    set useefect to handle alert timeout

--- src/Alert.js
+++ src/Alert.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react'
 
-const Alert = ({type, msg, removeAlert}) => {
+const Alert = ({type, msg, removeAlert, list}) => {
 
-  
+  useEffect(() => {
+
+  },[list])
   return (
     <p className={`alert alert-${type}`}>{msg}</p>
   )
